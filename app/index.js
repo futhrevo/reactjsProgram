87c003e3832cd0d02704ae0fb45325d55b207f62
Rakesh Kalyankar <k.rakeshlal@gmail.com>
2016-03-08 11:56:44 +0530

    lesson 4

--- app/index.js
+++ app/index.js
@@ -1,14 +1,7 @@
 var React = require('react');
 var ReactDom = require('react-dom');
-
-var HelloWorld = React.createClass({
-    render: function(){
-    	return(
-    		<div> Hello World </div>
-    		)
-    }
-});
+var routes = require('./config/routes');
 
 ReactDom.render(
-<HelloWorld /> , document.getElementById('app')
-)
+routes , document.getElementById('app')
+);
